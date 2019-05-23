82d302073f09c064b060cec7af29702eb256b372
unknown <samantha.c.egge@gmail.com>
2019-05-23 04:00:13 -0700

    Completed initial styling of the primary home content boxes. Added photos to asset folders for each selector. Began writing JS. Need Styling, Content, and JS finished.

---
+++ components/HomeContent/homecontent.js
@@ -0,0 +1,28 @@
+class BoxDisplay {
+    constructor(accordian){
+        this.accordian = accordian
+        this.image = accordian.querySelector('.book-img')
+        this.info = accordian.querySelector('.book-info')
+        // console.log('info', this.info)
+        // console.log('images', this.image)
+        // this.imagesArray = thi
+        this.image.addEventListener('click', () => this.select())
+    }
+
+   select(){
+        const allInfo = document.querySelectorAll('.book-info')
+        allInfo.forEach(info => {
+           info.classList.add('hidden-book');
+        })
+
+        this.info.classList.remove('hidden-book')
+    }
+}
+
+const accordianMaker = document.querySelectorAll('.book-container');
+// console.log(accordianMaker)
+
+accordianMaker.forEach((e,index) => {
+    const newAccord = new BoxDisplay(e)
+    // console.log('accordianmaker', newAccord, index)
+})
